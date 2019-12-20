262877c085581d06136de52db399f1d9dcea7804
R-second <R-second@yamadarigatsu-no-MacBook-Air.local>
2019-12-20 15:48:41 +0900

    finish set

--- convert.py
+++ convert.py
@@ -4,19 +4,19 @@
 import csv
 import pprint
 
-tone_fre = []
+note_quater = 12    # 基準にする音の長さ
+tone_fre = []       # 音階と周波数の対応リスト
 
+# 音階を引数に周波数を返す関数
 def search_in_frequency(x):
     #print(int(x))
     for row in tone_fre:
         if row[0] == x:
-            return row[1]
+            return [float(row[1]), float(row[2])]
     return -1
-        
 
 with open("data/tone_frequency.csv") as f:
     reader = csv.reader(f)
-
     for row in reader:
         tone_fre.append(row)
 
@@ -27,23 +27,40 @@ with open('data/data.csv') as f:
 
 
 
-data = [[] for row in l ]
+data = []
 
 
 i=0
 for row in l:
-
+    
     index = row[0].find('s.setTone(')
     tone = row[0][index+10:index+12]
-    data[i].append(tone)
 
     index = row[2].find(');')
-    data[i].append(row[2][index-2:index])
+    length = int(row[2][index-2:index])
 
-    data[i].append(search_in_frequency(tone))
+    fre_time = search_in_frequency(tone)
 
 
-    i = i + 1
+    if length == note_quater:
+        data.append(fre_time)
+        #data[i].append(tone)
+        #data[i].append(note_quater)
+        #data[i].append(frequency)
+        i = i + 1
+    else:
+        j = 0
+        while j < length / note_quater:
+            #data.append(fre_time)
+            data.append("CD     " + str(round(fre_time[0] / 2)) + "," + str(round(fre_time[1] * 100 / 4)))
+            #data[i].append(tone)
+            #data[i].append(note_quater)
+            #data[i].append(frequency)
+            i = i + 1
+            j = j + 1
+    
+
+
 
 print(data)
 
